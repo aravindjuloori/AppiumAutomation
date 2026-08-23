9ce4178e26c4b3233fcbb697e36f611ffb7e83e9
Aravind Juloori <julooriaravind@gmail.com>
2026-08-23 17:50:04 +0530

    Appium basic setup

--- Appium/src/test/java/appiumPractise/AppiumBasics.java
+++ Appium/src/test/java/appiumPractise/AppiumBasics.java
@@ -3,24 +3,22 @@ package appiumPractise;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import io.appium.java_client.AppiumBy;
 
 public class AppiumBasics extends BaseTest {
 
 	@Test
 	public void WifiSettings() {
-		driver.findElement(AppiumBy.accessibilityId("Preference")).click();    //Accessibility Id
-		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();  //Xpath
+
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click(); // Accessibility Id
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click(); // Xpath
 		driver.findElement(By.id("android:id/checkbox")).click();
 		driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
 		driver.findElement(By.id("android:id/edit")).sendKeys("Aravind5G wifi");
-		String popupTitle=driver.findElement(By.id("android:id/alertTitle")).getText();
-//		System.out.println(popupTitle);
+		String popupTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
+//	System.out.println(popupTitle);
 		Assert.assertEquals(popupTitle, "WiFi settings");
 		driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
-		
-	
-		
 	}
-
 }
--- Appium/src/test/java/appiumPractise/BaseTest.java
+++ Appium/src/test/java/appiumPractise/BaseTest.java
@@ -3,17 +3,11 @@ package appiumPractise;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.Duration;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
-import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
@@ -26,78 +20,39 @@ public class BaseTest {
 	public AppiumDriverLocalService service;
 
 	@BeforeClass
-	public void configureAppium() throws MalformedURLException, URISyntaxException {
+	public void configureAppium() throws MalformedURLException {
 
 		// Starting Appium server programatically
-
 		service = new AppiumServiceBuilder()
 				.withAppiumJS(new File(
 						"C:\\Users\\Aravind\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
 				.withIPAddress("127.0.0.1").usingPort(4723).build();
 
-		service.start(); // To start appium server
+		service.start();
 
 		UiAutomator2Options options = new UiAutomator2Options();
 		options.setDeviceName("10BE1Y0BZM0012Y");
-//		options.setDeviceName("AravindEmulator");
 		options.setPlatformName("Android");
-		options.setChromedriverExecutable("C:\\Users\\Aravind\\git\\Appium\\Appium\\src\\test\\java\\resources\\chromedriver.exe");
-		
-		
-//		options.setApp("C:\\Users\\Aravind\\git\\Appium\\Appium\\src\\test\\java\\resources\\ApiDemos-debug.apk");
-		options.setApp("C:\\Users\\Aravind\\git\\Appium\\Appium\\src\\test\\java\\resources\\General-Store.apk");
-		
+		options.setApp("C:\\Users\\Aravind\\git\\Appium\\Appium\\src\\test\\java\\resources\\ApiDemos-debug.apk");
+//		options.setApp("C:\\Users\\Aravind\\git\\Appium\\Appium\\src\\test\\java\\resources\\General-Store.apk");
+
+//		AndroidDriver driver=new AndroidDriver(new URL("http://127.0.0.1:4723"),options);  //if java version is below 20 it will work because java version above 20 URL class is deprecated
+//		AndroidDriver driver=new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(),options); 
 
 		// Android device or ios device
 		// appium code --> appium server
-		
-         URL appiumserverurl = URI.create("http://127.0.0.1:4723").toURL(); 
-		 driver=new AndroidDriver(appiumserverurl,options);
-		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-       
-		 
-		 //driver=new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(),options);
-		//driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);  // if java version is less than 20 version this will work above java 20 version use URI class
+
+		URL appiumserverurl = URI.create("http://127.0.0.1:4723").toURL();
+		driver = new AndroidDriver(appiumserverurl, options);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 	}
-
-	public void longPressAction(WebElement ele) {
-		
-		((JavascriptExecutor)driver).executeScript("mobile:longClickGesture",
-				ImmutableMap.of("elementId",((RemoteWebElement)ele).getId(),"duration",2000));
-	}
-
-	public void swipeAction(WebElement ele, String direction) {
-		((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of("elementId",
-				((RemoteWebElement) ele).getId(), "direction", direction, "percent", 0.75));
-	}
-
-	public void dragGesture(WebElement source, int endX, int endY) {
-		((JavascriptExecutor) driver).executeScript("mobile: dragGesture",
-				ImmutableMap.of("elementId", ((RemoteWebElement) source).getId(), "endX", endX, "endY", endY));
-
-	}
-
-	public void scrollToEndAction() {
-		boolean canScrollMore;
-		do {
-			canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap
-					.of("left", 100, "top", 100, "width", 200, "height", 200, "direction", "down", "percent", 3.0));
-
-		} while (canScrollMore);
-	}
-
-	public Double getFormattedAmount(String amount) {
-	Double price=Double.parseDouble(amount.substring(1));
-	return price;
-}
 
 	@AfterClass
-	public void tearDown() {
+	public void teardown() {
 
 		driver.quit();
 		service.stop(); // to stop the appium server
-
 	}
 
 }
